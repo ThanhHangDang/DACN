4e8b31a39da9ee93f79472da89132ddb61c0109c
nhantruongcse <nhan.truongcse@hcmut.edu.vn>
2025-02-08 22:26:46 +0700

    cap nhat phan register user

--- backend/src/models/authencationModels.js
+++ backend/src/models/authencationModels.js
@@ -17,4 +17,39 @@ const loginExecute = async (username, password) => {
   return rows[0];
 };
 
-module.exports = { findUserByUsername, loginExecute };
+const register_user = async (username, full_name, phone_number, email, password, role ) => {
+  const connection = await db.getConnection(); 
+  try {
+    console.log("role",role);
+    await connection.beginTransaction();
+    if (role==="3")     
+      { await db.query( 'CALL create_user_jobseeker(?,?,?,?,?,@p_status);',
+                        [username,full_name,phone_number,email, password]);}
+    else
+      { await db.query( 'CALL create_user_employer(?,?,?,?,?,@p_status);',
+                        [username,full_name,phone_number,email, password]);}
+    const [[{ newid }]] = await db.query('SELECT @p_status as newid;');
+    await connection.commit();
+    console.log(newid);
+    if (newid != 0) {
+      const [rows] = await db.query(
+        "SELECT * FROM user_ WHERE user_id = ?", [newid] );
+      return rows[0]; // return user mới tạo
+    }
+
+    return newid; // return 0 nếu không tạo được user
+  
+} catch (error) {
+  // Rollback nếu có lỗi
+  await connection.rollback();
+  console.error("Error logs hệ thống:", error);
+  return -1;
+}
+finally {
+  // Đóng connection để tránh rò rỉ
+  connection.release();
+}
+};
+
+module.exports = { findUserByUsername, loginExecute,register_user};
+
