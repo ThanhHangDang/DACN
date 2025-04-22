7b51cfd633a0eab356c3975562ac7fcbe7e30ac5
Pizon <thanhhangdang16@gmail.com>
2025-04-22 12:38:36 +0700

    fix some on JobCard Layout, 22/04/2025 12:38 PM

--- frontend/src/component/EmployerPage/CandidateMaganePage/index.js
+++ frontend/src/component/EmployerPage/CandidateMaganePage/index.js
@@ -44,7 +44,11 @@ export default function EmployeeMaganePage() {
                         className="text-truncate card-title"
                         style={{ maxWidth: "200px" }}
                       >
-                        {candidate.full_name}
+                        <NavLink
+                          to={`/candidate-detail/${candidate.jobseeker_id}`}
+                        >
+                          {candidate.full_name}
+                        </NavLink>
                       </h5>
                       <p className="card-text">{candidate.title}</p>
                       <p className="text-muted">
--- frontend/src/component/_component/ui/JobCard.js
+++ frontend/src/component/_component/ui/JobCard.js
@@ -22,27 +22,51 @@ const JobCard = ({ job }) => {
           </span>
           <i className="bi bi-bookmark"></i>
         </div>
-        <h5 className="card-title fw-bold">{job?.title}</h5>
-        <p className="card-text mb-2">{job?.company_name}</p>
-        <div className="d-flex flex-wrap gap-3 mb-2">
-          <span className="badge bg-light text-dark">
-            {job?.job_function_name}
-          </span>
-          <span className="badge bg-light text-dark">{job?.working_type}</span>
-          <span className="badge bg-light text-dark">
-            {job?.salary_max} - {job?.salary_min}
-          </span>
-          <span className="badge bg-light text-dark">
-            <i className="bi bi-geo-alt-fill me-1"></i>
-            {job?.work_location_name}
-          </span>
+        <div className="d-flex justify-content-start">
+          <div>
+            <img
+              src={job?.company_logo || job?.logo}
+              alt="Logo"
+              className="img-fluid me-2 rounded-1 me-3"
+              style={{ width: 80, height: 80 }}
+            />
+          </div>
+          <div>
+            <h5 className="card-title fw-bold">
+              <NavLink to={`/post-detail/${job?.job_id}`}>{job?.title}</NavLink>
+            </h5>
+            <p className="card-text mb-2">
+              <NavLink
+                to={`/company-detail/${job?.company_id || job?.employer_id}`}
+                className="text-decoration-none custom-hover-3"
+              >
+                {job?.company_name}
+              </NavLink>
+            </p>
+            <div className="d-flex flex-wrap gap-3 mb-2">
+              <span className="badge bg-light text-dark">
+                {job?.job_function_name}
+              </span>
+              <span className="badge bg-light text-dark">
+                {job?.working_type}
+              </span>
+              <span className="badge bg-light text-dark">
+                {job?.salary_max} - {job?.salary_min}
+              </span>
+              <span className="badge bg-light text-dark">
+                <i className="bi bi-geo-alt-fill me-1"></i>
+                {job?.work_location_name}
+              </span>
+            </div>
+          </div>
         </div>
-        <NavLink
+
+        {/* <NavLink
           to={`/post-detail/${job?.job_id}`}
           className="btn btn-success btn-sm"
         >
           Job Details
-        </NavLink>
+        </NavLink> */}
       </div>
     </div>
   );
